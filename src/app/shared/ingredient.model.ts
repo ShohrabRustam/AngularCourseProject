7073bb0172c33cd8a0e0c39ff7538943cb54fe0d
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-02 17:29:07 +0530

    add the ingredient model for the shared shopimh list and recipies

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient{
+public name:string;
+public amount:number;
+constructor(name:string,amount:number){
+this.name=name;
+this.amount=amount;
+}
+
+// we can make also like that
+
+// constructor(public name:string,public amount:number){
+
+// }
+}
