ae3055d7b835a926f977448b6fdc2fc54608c577
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-02 16:46:06 +0530

    add recipies data from the recipe-list itme

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// import Recipe Models
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes=[];
+  recipes:Recipe[]=[
+    new Recipe('A test Recipe ', 'This is simple a test ', 'https://miro.medium.com/max/1360/0*DaqUDBK-6UrgG5fa.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
